28c9d9d468f73bbae42da63e346740cdb2259134
imanardiansyahh <imanardiansyah89@gmail.com>
2020-12-23 14:46:09 +0800

    third

--- concatArr.js
+++ concatArr.js
@@ -2,3 +2,18 @@ var items = [1, 2,'a','b','c']
 var newItems = items.concat(3,4,5)
 
 console.log(newItems)
+
+
+var people = [
+    {
+        name : 'iman'
+    }
+    ,
+    {
+        name : 'ardiansyah'
+    }
+]
+
+people.forEach(function(person) {
+    console.log(person.name)
+})
