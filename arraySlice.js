423f2422b4357a97481c70db552cced971f6dc8b
imanardiansyahh <imanardiansyah89@gmail.com>
2020-12-27 22:00:24 +0800

    waktu

---
+++ arraySlice.js
@@ -0,0 +1,10 @@
+// var person = {name : 'shane'}
+var items = [1, 2, 3, 4, 5, 6, 7, 8]
+var copy = items.slice(1, 7)
+
+// copy.push(8)
+// copy[1].name= 'Sally'
+// copy[2] = 7
+
+
+console.log(copy)
